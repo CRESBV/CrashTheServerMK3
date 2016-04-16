934f676ed8beb323e205da1a73fbf578ca4d169f
Ed Buckler <esb33@cornell.edu>
2016-04-16 15:15:19 -0400

    Added code for making data table, still need to handle edge cases

---
+++ src/GetAllTheDataV2.java
@@ -0,0 +1,103 @@
+/**
+ * Created by Edward on 4/13/2016.
+ */
+import com.google.common.collect.Table;
+import com.google.common.collect.TreeBasedTable;
+import com.google.common.primitives.Doubles;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.plnyyanks.tba.apiv2.APIv2Helper;
+import com.plnyyanks.tba.apiv2.interfaces.APIv2;
+import com.plnyyanks.tba.apiv2.models.Event;
+import com.plnyyanks.tba.apiv2.models.Match;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by edbuckler on 4/9/16.
+ */
+public class GetAllTheDataV2 {
+
+    //private static final String API_APP_ID = "plnyyanks:apiv2-java-junit:v0.1";
+    private static final String API_APP_ID = "frc639:scouting-system:v01";
+    //https://docs.google.com/spreadsheets/d/1HqsReMjr5uBuyZjqv14t6bQF2n038GfMmWi3B6vFGiA/edit#gid=24
+
+    public static void main(String[] args) {
+        APIv2Helper.setAppId(API_APP_ID);
+        APIv2 api = APIv2Helper.getAPI();
+        System.out.println("Hello World");
+//        List<District> districts = api.fetchDistrictList(2016, null);
+        // List<Event> events = api.fetchDistrictEvents("in", 2016, null);
+        List<Event> events = api.fetchEventsInYear(2016,null);
+        events.stream().forEach(ev -> System.out.println(ev.getKey()));
+//        Match matches=api.fetchMatch("2016nyro_f1m1",null);
+//        System.out.println(matches.getKey()+matches.getAlliances().toString());
+//        System.out.println(matches.getScore_breakdown().toString());
+
+        Table<String,String,List<Double>> dataTable= TreeBasedTable.create();
+        for (Event event : events) {
+            List<Match> matchList=api.fetchEventMatches(event.getKey(),null);
+            matchList.stream().limit(100)
+                    .forEach(match -> {
+                        String teamsInString=match.getAlliances().getAsJsonObject("blue").toString()+
+                                match.getAlliances().getAsJsonObject("red").toString();
+                        List<String> blueTeams= new ArrayList<>();
+                        for (JsonElement blueTeam : match.getAlliances().getAsJsonObject("blue").getAsJsonArray("teams")) {
+                            blueTeams.add(blueTeam.getAsString());
+                        }
+                        Double score=match.getAlliances().getAsJsonObject("blue").get("score").getAsDouble();
+                        System.out.println(blueTeams.toString());
+
+                        for (String blueTeam : blueTeams) {
+                            List<Double> values=dataTable.get(blueTeam,"score");
+                            if(values==null) {
+                                values=new ArrayList<>();
+                            }
+                            values.add(score);
+                            dataTable.put(blueTeam,"score",values);
+                        }
+                        JsonObject scoreBreak=null;
+                        try{
+                            scoreBreak=match.getScore_breakdown().getAsJsonObject().getAsJsonObject("blue");
+                        } catch (IllegalStateException ise) {
+                            System.err.println(match.toString());
+                        }
+                        if(scoreBreak==null) return;
+                        for (String blueTeam : blueTeams) {
+                            scoreBreak.entrySet().stream()
+                                    .forEach(es -> {
+                                        System.out.println(es.toString());
+                                        Double value=Double.NaN;
+                                        String valueAsString=es.getValue().getAsString();
+                                        if(Doubles.tryParse(valueAsString)!=null) {
+                                            value=Double.parseDouble(valueAsString);
+                                        } else if(valueAsString.equals("true")) {
+                                            value=1.0;
+                                        } else if(valueAsString.equals("false")) {
+                                            value=0.0;
+                                        } else if(valueAsString.equals("Challenged")) {
+                                            value=1.0;
+                                        }  else if(valueAsString.equals("None")) {
+                                            value=0.0;
+                                        } else {
+                                            value=Double.NaN;
+                                        }
+                                        List<Double> values = dataTable.get(blueTeam, es.getKey());
+                                        if (values == null) {
+                                            values = new ArrayList<>();
+                                        }
+                                        values.add(value);
+                                        dataTable.put(blueTeam, es.getKey(), values);
+                                    });
+                        }
+
+                    });
+            break;
+        }
+        System.out.println(dataTable.toString());
+
+    }
+
+
+}
