93f504d93c6fd20f2d4ca9cf6e6dcc1696af82a2
Ed Buckler <esb33@cornell.edu>
2016-04-25 16:09:02 -0400

    Initial support for power scores

--- src/GetAllTheDataV2.java
+++ src/GetAllTheDataV2.java
@@ -17,11 +17,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * Created by edbuckler on 4/9/16.
@@ -33,45 +31,55 @@ public class GetAllTheDataV2 {
     //https://docs.google.com/spreadsheets/d/1HqsReMjr5uBuyZjqv14t6bQF2n038GfMmWi3B6vFGiA/edit#gid=24
 
     public static void main(String[] args) {
+        //args restrict by event and/or by team
         APIv2Helper.setAppId(API_APP_ID);
         APIv2 api = APIv2Helper.getAPI();
         System.out.println("Hello World");
 //        List<District> districts = api.fetchDistrictList(2016, null);
         // List<Event> events = api.fetchDistrictEvents("in", 2016, null);
         List<Event> events = api.fetchEventsInYear(2016,null);
-        events.stream().forEach(ev -> System.out.println(ev.getKey()));
+        events.stream().forEach(ev -> System.out.println(ev.getKey()+"\t"+ev.getStart_date()));
+
 //        Match matches=api.fetchMatch("2016nyro_f1m1",null);
 //        System.out.println(matches.getKey()+matches.getAlliances().toString());
 //        System.out.println(matches.getScore_breakdown().toString());
 
         Table<String,String,List<Double>> dataTable= TreeBasedTable.create();
-        for (Event event : events) {
-            List<Match> matchList=api.fetchEventMatches(event.getKey(),null);
-            matchList.stream()//.limit(10)
-                    .forEach(match -> {
-                        List<String> allianceColors = Lists.newArrayList("blue", "red");
-                        for (String allianceColor : allianceColors) {
-                            List<String> oneAlliance = new ArrayList<>();
-                            for (JsonElement blueTeam : match.getAlliances().getAsJsonObject(allianceColor).getAsJsonArray("teams")) {
-                                oneAlliance.add(blueTeam.getAsString());
-                            }
+        Table<String,String,Integer> powerTable= TreeBasedTable.create();
+        Map<String, Double> attributeSum = new TreeMap<>();
 
-                            JsonObject scoreBreak = null;
-                            JsonObject scoreBreakDef = null;
-                            try {
-                                scoreBreak = match.getScore_breakdown().getAsJsonObject().getAsJsonObject(allianceColor);
-                                //opposite
-                                scoreBreakDef = match.getScore_breakdown().getAsJsonObject().getAsJsonObject(allianceColor.equals("blue")?"red":"blue");
-                            } catch (IllegalStateException ise) {
-                                System.err.println("Match scores failing for:"+match.getKey().toString());
-                            }
-                            if (scoreBreak == null || scoreBreakDef==null) return;
-                            addScoresToDataTable(dataTable, scoreBreak, oneAlliance,true);
-                            addScoresToDataTable(dataTable, scoreBreakDef, oneAlliance,false);
-                        }
-                    });
-        }
+        events.stream()
+                .limit(5)
+                .forEach(event -> {
+                    List<Match> matchList = api.fetchEventMatches(event.getKey(), null);
+                    matchList.stream()//.limit(10)
+                            .forEach(match -> {
+                                List<String> allianceColors = Lists.newArrayList("blue", "red");
+                                for (String allianceColor : allianceColors) {
+                                    List<String> oneAlliance = new ArrayList<>();
+                                    for (JsonElement blueTeam : match.getAlliances().getAsJsonObject(allianceColor).getAsJsonArray("teams")) {
+                                        oneAlliance.add(blueTeam.getAsString());
+                                    }
+
+                                    JsonObject scoreBreak = null;
+                                    JsonObject scoreBreakDef = null;
+                                    try {
+                                        scoreBreak = match.getScore_breakdown().getAsJsonObject().getAsJsonObject(allianceColor);
+                                        //opposite
+                                        scoreBreakDef = match.getScore_breakdown().getAsJsonObject().getAsJsonObject(allianceColor.equals("blue") ? "red" : "blue");
+                                    } catch (IllegalStateException ise) {
+                                        System.err.println("Match scores failing for:" + match.getKey().toString());
+                                    }
+                                    if (scoreBreak == null || scoreBreakDef == null) return;
+                                    addScoresToDataTable(dataTable, scoreBreak, oneAlliance, true);
+                                    addScoresToDataTable(dataTable, scoreBreakDef, oneAlliance, false);
+                                    addScoresToPowerTable(powerTable, attributeSum, scoreBreak, oneAlliance, "totalPoints", true);
+                                }
+                            });
+                });
+
         System.out.println(makeNiceLookingTable(dataTable));
+
         Path output = Paths.get("/Users/edbuckler/Downloads/CrashTheServerOutput.txt");
         try {
             Files.write(output, makeNiceLookingTable(dataTable).getBytes());
@@ -81,6 +89,53 @@ public class GetAllTheDataV2 {
         //System.out.println(dataTable.toString());
 
     }
+
+    private static void estimatePowerScores(Table<String,String,Integer> powerTable, Map<String, Double> attributeSum) {
+
+    }
+
+    private static void addScoresToPowerTable(Table<String,String,Integer> powerTable, Map<String, Double> attributeSum,
+                                              JsonObject scoreBreak, List<String> oneAlliance, String attribute, boolean offenseOrDefence) {
+        final String prefix=(offenseOrDefence)?"Off_":"Def_";
+        Map<String,String> positionMap=createPositionMap(scoreBreak);
+        positionMap.put("position1crossings","Low_Bar");
+
+        scoreBreak.entrySet().stream()
+                .filter(es -> es.getKey().equals(attribute))
+                .forEach(es -> {
+                    String finalKey=prefix+es.getKey();
+                    Double finalValue=Double.NaN;
+                    String valueAsString=es.getValue().getAsString();
+                    if(Doubles.tryParse(valueAsString)!=null) {
+                        finalValue=Double.parseDouble(valueAsString);
+                    } else if(valueAsString.equals("true")) {
+                        finalValue=1.0;
+                    } else if(valueAsString.equals("false")) {
+                        finalValue=0.0;
+                    } else if(valueAsString.equals("Challenged") || valueAsString.equals("Scaled") || valueAsString.equals("Both")) {
+                        finalValue=1.0;
+                    }  else if(valueAsString.equals("Unknown")) {
+                        finalValue=0.0;
+                    }  else if(valueAsString.equals("None")) {
+                        finalValue=0.0;
+                    } else {
+                        finalValue=Double.NaN;
+                    }
+                    if(Double.isNaN(finalValue)) return;
+                    final double fv=finalValue;
+                    for (String aTeam : oneAlliance) {
+                        for (String bTeam : oneAlliance) {
+                            Integer value=powerTable.get(aTeam,bTeam);
+                            if(value==null) {value=1;}
+                            else {value=value+1;}
+                            powerTable.put(aTeam, bTeam, value);
+                        }
+                        attributeSum.merge(aTeam, fv, (ov, nv) -> ov + nv);
+                    }
+                });
+
+    }
+
 
     private static void addScoresToDataTable(Table<String,String,List<Double>> dataTable, JsonObject scoreBreak,
                                              List<String> oneAlliance, boolean offenseOrDefence) {
